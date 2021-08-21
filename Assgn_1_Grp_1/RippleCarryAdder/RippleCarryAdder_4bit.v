8266e4cca3b88cf663c70dc828be73ae70155b5d
Nisarg Upadhyaya <nisarg1631@gmail.com>
2021-08-22 01:05:52 +0530

    add 4bit rca

---
+++ Assgn_1_Grp_1/RippleCarryAdder/RippleCarryAdder_4bit.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+
+//////////////////////////////////////////////////////////////////////////////////
+/*
+    Assignment Number: 1
+    Problem Number: 1 Ripple Carry Adder
+    Semester Number: 5
+    Group Number: 1 
+    Group Members: Animesh Jha(19CS10010), Nisarg Upadhyaya (19CS30031)
+*/
+//////////////////////////////////////////////////////////////////////////////////
+
+module RippleCarryAdder_4bit(
+    input [3:0] in1,
+    input [3:0] in2,
+    input c_in,
+    output [3:0] sum,
+    output c_out
+    );
+
+	wire [2:0] carry;
+	// cascade 4 FullAdders using carry wire
+	FullAdder fa1(in1[0], in2[0], c_in, sum[0], carry[0]);
+   FullAdder fa2(in1[1], in2[1], carry[0], sum[1], carry[1]);
+   FullAdder fa3(in1[2], in2[2], carry[1], sum[2], carry[2]);
+   FullAdder fa4(in1[3], in2[3], carry[2], sum[3], c_out);
+
+endmodule
---
+++ Assgn_1_Grp_1/RippleCarryAdder/RippleCarryAdder_4bitTestbench.v
@@ -0,0 +1,44 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+/*
+    Assignment Number: 1
+    Problem Number: 1 Ripple Carry Adder
+    Semester Number: 5
+    Group Number: 1 
+    Group Members: Animesh Jha(19CS10010), Nisarg Upadhyaya (19CS30031)
+*/
+////////////////////////////////////////////////////////////////////////////////
+
+module RippleCarryAdder_4bitTestbench;
+
+	// Inputs
+	reg [3:0] in1;
+	reg [3:0] in2;
+	reg c_in;
+
+	// Outputs
+	wire [3:0] sum;
+	wire c_out;
+
+	// Instantiate the Unit Under Test (UUT)
+	RippleCarryAdder_4bit uut (
+		.in1(in1), 
+		.in2(in2), 
+		.c_in(c_in), 
+		.sum(sum), 
+		.c_out(c_out)
+	);
+
+	initial begin
+		// Initialize Inputs
+		in1 = 4'b0100; in2 = 4'b0100; c_in = 0;
+		#100;
+		in1 = 4'd0100; in2 = 4'b1100; c_in = 0;
+        
+		$monitor ("in1 = %d, in2 = %d, c_in = %d, sum = %d, c_out = %d", in1, in2, c_in, sum, c_out);
+
+	end
+      
+endmodule
+
