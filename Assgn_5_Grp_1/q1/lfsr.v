41a84510778953f18644c3add5f2ed3758284877
JhadotAnimesh <jha.animesh01@gmail.com>
2021-09-27 00:59:49 +0530

    smol lfsr maybe wrong

--- Assgn_5_Grp_1/q1/lfsr.v
+++ Assgn_5_Grp_1/q1/lfsr.v
@@ -1,3 +1,5 @@
+
+`timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 // Assignment Number: 5
 // Problem Number: 1 [Linear Feedback Shift Register (LFSR)]
@@ -7,3 +9,26 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
+module linear_feedback_shift_register(
+    input clk,
+    input reset,
+    input [3:0] seed,
+    output [3:0] out,
+);
+    reg [3:0] state;
+    reg [3:0] feedback;
+    always @(posedge clk) 
+    begin
+        if (reset) 
+        begin
+            state <= seed;
+        end 
+        else 
+        begin
+            feedback <= state[3] ^ state[2];
+            state <= state[2:0] << 1;
+            state[0] <= feedback;
+        end
+    end
+    assign out = state;
+endmodule
