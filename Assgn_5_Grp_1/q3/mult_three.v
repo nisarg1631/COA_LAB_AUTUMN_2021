def0d57ef08f9f097c8d4b97719a29811649a05c
Nisarg Upadhyaya <nisarg1631@gmail.com>
2021-09-28 14:16:40 +0530

    q3 done

--- Assgn_5_Grp_1/q3/mult_three.v
+++ Assgn_5_Grp_1/q3/mult_three.v
@@ -14,60 +14,47 @@ module multiple_of_three_detector_fsm(
     input clk,              // clock input
     input in                // input to the FSM
 );
-    reg[1:0] present_state,next_state; // 3 states so 2 flip-flop
-    localparam rem0 = 0;
-    localparam rem1 = 1;
-    localparam rem2 = 2;
+    reg[1:0] present_state, next_state; // 3 states so 2 flip-flop
+    localparam rem0 = 2'b00, rem1 = 2'b01, rem2 = 2'b10; // 3 states representing number modulo 3
 
-    always @(posedge clk) 
-    begin
-        present_state = next_state;
-        if (reset) 
-        begin
-            next_state=rem0;
-            out=0;
+    always @(posedge clk) begin
+        present_state = next_state;     // switch state
+        if (reset) begin                // if reset is set make next state 0 and output 1
+            next_state <= rem0;
+            out <= 1'b1;
         end 
-        else 
-        begin
-            if (present_state==rem0) 
-            begin
-                if(in==0) 
-                begin
-                    next_state=rem0;
-                    out=1;
+        else begin
+            if (present_state == rem0) begin    // if number read till now is of type 3n
+                if(in == 1'b0) begin            // 2*3n + 0 = 6*n = 0 (mod 3), stay in state 0, output 1
+                    next_state <= rem0;
+                    out <= 1'b1;
                 end
-                else if(in==1) 
-                begin
-                    next_state=rem1;
-                    out=0;
+                else if(in == 1'b1) begin       // 2*3n + 1 = 6*n + 1 = 1 (mod 3), move to state 1, output 0
+                    next_state <= rem1;
+                    out <= 1'b0;
                 end 
             end
-            else if (present_state==rem1)
-            begin
-                if(in==0) 
-                begin
-                    next_state=rem2;
-                    out=0;
+            else if (present_state == rem1) begin   // if number read till now is of type 3n+1
+                if(in == 1'b0) begin                // 2*(3n+1) + 0 = 6*n + 2 = 2 (mod 3), move to state 2, output 0
+                    next_state <= rem2;
+                    out <= 1'b0;
                 end
-                else if(in==1) 
-                begin
-                    next_state=rem0;
-                    out=1;
+                else if(in == 1'b1) begin           // 2*(3n+1) + 1 = 6*n + 3 = 0 (mod 3), move to state 0, output 1
+                    next_state <= rem0;
+                    out <= 1'b1;
                 end
             end          
-            else if(present_state==rem2)
-            begin
-                if(in==0) 
-                begin
-                    next_state=rem1;
-                    out=0;
+            else if(present_state == rem2) begin    // if number read till now is of type 3n+2
+                if(in == 1'b0) begin                // 2*(3n+2) + 0 = 6*n + 4 = 1 (mod 3), move to state 1, output 0
+                    next_state <= rem1;
+                    out <= 1'b0;
                 end 
-                else if(in==1) 
-                begin
-                    next_state=rem2;
-                    out=0;
+                else if(in == 1'b1) begin           // 2*(3n+2) + 1 = 6*n + 5 = 2 (mod 3), move to state 2, output 0
+                    next_state <= rem2;
+                    out <= 1'b0;
                 end
             end  
         end
     end
+
 endmodule
--- Assgn_5_Grp_1/q3/tb.v
+++ Assgn_5_Grp_1/q3/tb.v
@@ -12,12 +12,13 @@
 module tb;
 
 	// Inputs
-	reg in;
-	reg clk=1;
-	reg reset=0;
+	reg in = 1'b0;
+	reg clk = 1'b1;
+	reg reset = 1'b0;
+	reg[31:0] decimal_num = 32'd0;
 	// Outputs
 	wire out;
-	always #40 clk = ~clk;
+	always #4 clk = ~clk;
 
 	// Instantiate the Unit Under Test (UUT)
 	multiple_of_three_detector_fsm uut(
@@ -26,21 +27,26 @@ module tb;
 		.clk(clk),
 		.reset(reset)
 	);
+	
 	initial begin
-	// Initialize Inputs
-    $monitor($time,"\t in = %b \t out = %b", in, out);
-	reset=1;
-	#40 reset=0;
-	#40 in = 0;
-   	#80 in = 1;
-   	#80 in = 1;
-   	#80 in = 0;
-   	#80 in = 1;
-   	#80 in = 1;
-   	#80 in = 0;
-   	#80 in = 1; 
-   	#80 $finish; 
+		reset=1;
+		#4 reset = 0;
+		#4
+
+		#8 in = 1'b1; decimal_num = 32'd1;
+		#8 in = 1'b0; decimal_num = 32'd2;
+		#8 in = 1'b0; decimal_num = 32'd4;
+		#8 in = 1'b1; decimal_num = 32'd9;
+		#8 in = 1'b0; decimal_num = 32'd18;
+		#8 in = 1'b1; decimal_num = 32'd37;
+		#8 in = 1'b0; decimal_num = 32'd74;
+		#8 in = 1'b1; decimal_num = 32'd149;
+		#8 in = 1'b0; decimal_num = 32'd298;
+		#8 in = 1'b1; decimal_num = 32'd597;
+		
+   		#8 $finish; 
 	end
+	always #8 $monitor($time,"\t new input bit = %b, \t number read till now = %d, \t divisibility by 3 = %b", in, decimal_num, out);
       
 endmodule
 
