3839465a03b042d2cafe198851be531b0f4403e1
Nisarg Upadhyaya <nisarg1631@gmail.com>
2021-09-28 18:15:30 +0530

    q4 done

--- Assgn_5_Grp_1/q4/suc.v
+++ Assgn_5_Grp_1/q4/suc.v
@@ -20,43 +20,53 @@ module sequential_unsigned_comparator (
     output reg E,
     output reg G
 );
-    reg [1:0] present_state,next_state;
+    reg [1:0] present_state, next_state;
+
     localparam s0 = 2'b00;       // equal state
-    localparam s1 = 2'b01;         // greater than state
-    localparam s2 = 2'b10;          // less than state
-    always @(posedge clk) 
-    begin
-		present_state=next_state;   
+    localparam s1 = 2'b01;       // greater than state
+    localparam s2 = 2'b10;       // less than state
+
+    always @(posedge clk) begin   
         if(rst) begin             // reset state
-            present_state = 0;
-            next_state=0;
+            present_state <= s0;
         end
-        if (present_state==s0)   // FSM logic 
-        begin
-            if(a^b)             // if a and b not the same
-                next_state=(a?s1:s2); // go to greater if a=1 and b=0 else go to less than
-        end            
-    end 
-    always @(posedge clk) begin
-        if(op==0) begin   //input control logic
-            L=0;
-            E=0;
-            G=0;
-        end
-        else if(next_state==s0) begin //output logic for equal state
-            L=0;
-            E=1;
-            G=0;
-        end
-        else if(next_state==s1) begin  //output logic for greater than state
-            L=0;
-            E=0;
-            G=1;
-        end
-        else if(next_state==s2) begin  //output logic for less than state
-            L=1;
-            E=0;
-            G=0;
+        else begin
+            present_state <= next_state;
         end
     end
+
+    // next state logic
+    always @(*) begin
+        if (present_state == s0 && a^b) begin // state change can only take place when from equal state                        
+            next_state = (a ? s1 : s2);       // if a and b not the same go to greater if a=1 and b=0 else go to less than
+        end
+        else begin
+            next_state = present_state;       // otherwise continue to stay in the present state
+        end
+    end
+
+    // store output in L, E, G
+    always @(*) begin
+        if(op == 1'b0) begin            //output control logic
+            L = 1'b0;
+            E = 1'b0;
+            G = 1'b0;
+        end
+        else if(next_state == s0) begin //output logic for equal state
+            L = 1'b0;
+            E = 1'b1;
+            G = 1'b0;
+        end
+        else if(next_state == s1) begin  //output logic for greater than state
+            L = 1'b0;
+            E = 1'b0;
+            G = 1'b1;
+        end
+        else begin  //output logic for less than state
+            L = 1'b1;
+            E = 1'b0;
+            G = 1'b0;
+        end
+    end
+
 endmodule
--- Assgn_5_Grp_1/q4/tb.v
+++ Assgn_5_Grp_1/q4/tb.v
@@ -15,8 +15,7 @@ module tb_suc;
 	reg clk = 1'b1;
 	reg reset = 1'b0;
 	reg opt=1'b0;
-	reg[31:0] decimal_num_1 = -1*32'd1;
-    reg[31:0] decimal_num_2 = 32'd123;
+	reg[31:0] decimal_num_1,  decimal_num_2;
 	wire a,b,L,E,G;
 	always #4 clk = ~clk;
 
@@ -25,10 +24,58 @@ module tb_suc;
     shift_register psr2(.clk(clk), .reset(reset), .inp(decimal_num_2), .out(b));
 	sequential_unsigned_comparator uut(.a(a),.b(b),.L(L),.E(E),.G(G),.clk(clk),.rst(reset),.op(opt));
 	initial begin
-		reset = 1'b1;
-		#4 reset = 1'b0;
-		#264 opt=1'b1; // 33 clockcycles lsb has been read by now
+	
+		$monitor($time,"\t L= %b,\t E= %b\t G= %b ", L, E, G);
+		
+		$display("Test A = 1234 and B = 1234\n");
+		decimal_num_1 = 32'd1234; decimal_num_2 = 32'd1234;
+		reset = 1'b1; opt = 1'b0;
+		#8 reset = 1'b0;
+		#264 opt = 1'b1; // 33 clockcycles lsb has been read by now
+		
+		#8
+		$display("Test A = 123456789 and B = 123\n");
+		decimal_num_1 = 32'd123456789; decimal_num_2 = 32'd123;
+		reset = 1'b1; opt = 1'b0;
+		#8 reset = 1'b0;
+		#264 opt = 1'b1; // 33 clockcycles lsb has been read by now
+
+		#8
+		$display("Test A = 9 and B = 99\n");
+		decimal_num_1 = 32'd9; decimal_num_2 = 32'd99;
+		reset = 1'b1; opt = 1'b0;
+		#8 reset = 1'b0;
+		#264 opt = 1'b1; // 33 clockcycles lsb has been read by now
+
+		#8
+		$display("Test A = 255 and B = 255\n");
+		decimal_num_1 = 32'd255; decimal_num_2 = 32'd255;
+		reset = 1'b1; opt = 1'b0;
+		#8 reset = 1'b0;
+		#264 opt = 1'b1; // 33 clockcycles lsb has been read by now
+		
+		#8
+		$display("Test A = 16161615 and B = 16161616\n");
+		decimal_num_1 = 32'd16161615; decimal_num_2 = 32'd16161616;
+		reset = 1'b1; opt = 1'b0;
+		#8 reset = 1'b0;
+		#264 opt = 1'b1; // 33 clockcycles lsb has been read by now
+
+		#8
+		$display("Test A = 15 and B = 14\n");
+		decimal_num_1 = 32'd15; decimal_num_2 = 32'd14;
+		reset = 1'b1; opt = 1'b0;
+		#8 reset = 1'b0;
+		#264 opt = 1'b1; // 33 clockcycles lsb has been read by now
+
+		#8
+		$display("Test A = 0 and B = 0\n");
+		decimal_num_1 = 32'd0; decimal_num_2 = 32'd0;
+		reset = 1'b1; opt = 1'b0;
+		#8 reset = 1'b0;
+		#264 opt = 1'b1; // 33 clockcycles lsb has been read by now
+
+		#8 $finish;
 	end
-	always #8 $monitor($time,"\t a = %b, \t b = %b \t L= %b,\t E= %b\t G= %b ", a, b, L, E, G);
       
 endmodule
