c29714ab30bf008af7c0d010f8d456ec44ec98c6
JhadotAnimesh <jha.animesh01@gmail.com>
2021-10-09 22:09:44 +0530

    remove circle shift

--- Assign_6_Grp_1/Q1/bi_bar_shift.v
+++ Assign_6_Grp_1/Q1/bi_bar_shift.v
@@ -16,10 +16,10 @@ module Bidirectional_Barrel_Shifter(
     output [7:0] out
 );
     reg [7:0] reg_temp,temp2;
-    mux Layer1(.select(dir),.x({in[6:0],in[7]}),.y({in[0],in[7:1]}),.out(reg_temp));  //  shift by 1 to left or right
+    mux Layer1(.select(dir),.x({in[6:0],1'b0}),.y({1'b0,in[7:1]}),.out(reg_temp));  //  shift by 1 to left or right
     mux Layer2(.select(shamt[0]),.x(in),.y(reg_temp),.out(out));    // to shift or not shift  by 1 
-    mux Layer3(.select(dir),.x({out[5:0],out[7:6]}),.y(out[1:0],out[7:2]),.out(reg_temp)); // shift by 2 to left or right
+    mux Layer3(.select(dir),.x({out[5:0],2'b0}),.y(2'b0,out[7:2]),.out(reg_temp)); // shift by 2 to left or right
     mux Layer4(.select(shamt[1]),.x(out),.y(reg_temp),.out(temp2)); // one more temp needed?  // to shift or not shift by 2
-    // direction does not matter for shifts of size 4 so skipped that layer 
-    mux Layer5(.select(shamt[2]),.x(temp2),.y({temp2[3:0],temp2[7:4]}),.out(out));    // to shift or not shift by 4
+    mux Layer5(.select(dir),.x({out[3:0],4'b0}),.y({4'b0,out[7:4]}),.out(reg_temp)); // shift by 4 to left or right
+    mux Layer6(.select(shamt[2]),.x(temp2),.y(reg_temp),.out(out));    // to shift or not shift by 4
 endmodule
