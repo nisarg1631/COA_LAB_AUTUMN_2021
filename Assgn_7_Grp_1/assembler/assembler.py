6fe849fd129283a255f79e19c7b55643baebcff5
JhadotAnimesh <jha.animesh01@gmail.com>
2021-11-07 17:07:29 +0530

    most done

--- Assgn_7_Grp_1/assembler/assembler.py
+++ Assgn_7_Grp_1/assembler/assembler.py
@@ -4,11 +4,58 @@ import json
 
 REGDICT = {}
 INSTRUCTION_DICT = {}
-TYPE_DICT = {}
 
 
 def spit_line(line):
-    pass
+    try:
+        print(f"processing {line}")
+        opcode=INSTRUCTION_DICT[line[0]][0]
+        opc=int(opcode,2)
+        print(opc)
+        if opc==1 or opc==2:
+            if len(line)!=3:
+                print("error")
+                return
+            else:
+                rs=f"{REGDICT[line[1]]:05b}"
+                rt=f"{REGDICT[line[2]]:05b}"
+                shamt=f"{0:010b}"
+                funct=f"{INSTRUCTION_DICT[line[0]][-1]}"
+                print(f"{opcode} {rs} {rt} {shamt} {funct}")
+        elif opc==3:
+            if len(line)!=3:
+                print("error")
+                return
+            else:
+                rs=f"{REGDICT[line[1]]:05b}"
+                funct=f"{INSTRUCTION_DICT[line[0]][-1]}"
+                print(f"{opcode} {rs}  {funct}")
+        elif opc==4:
+            if len(line)<2:
+                print("error")
+                return
+            else:
+                funct=f"{INSTRUCTION_DICT[line[0]][-1]}"
+                rs=f"{REGDICT[line[1]]:05b}"
+                addr=f"{0:015b}"
+                if int(funct)!=0:
+                    if len(line)!=3:
+                        print("error")
+                        return
+                    else:
+                        addr=f"{int(line[2]):015b}"
+                print(f"{opcode} {rs} {addr} {funct}")
+        elif opc==5:
+            if len(line)!=2:
+                print("error")
+                return
+            else:
+                funct = f"{INSTRUCTION_DICT[line[0]][-1]}"
+                rs=f"{0:05b}"
+                addr=f"{int(line[1]):015b}"
+                print(f"{opcode} {rs} {addr} {funct}")
+    except:
+        print("error")
 
 
 def bin_comm(string):
@@ -18,24 +65,21 @@ def bin_comm(string):
 
 
 def process(filename):
+    print(f"{0:032b}")
     with open(filename, 'r') as f:
         lines = f.readlines()
         for line in lines:
             line.strip()
             line = bin_comm(line)
-            line = line.split()
+            
+            line = line.replace(',',' ').split()
             if len(line):
                 spit_line(line)
-
+    print(f"{0:032b}")
 
 if __name__ == '__main__':
     with open('instruc.json', 'r') as f:
         INSTRUCTION_DICT = json.load(f)
-    with open('register.json', 'r') as f:
+    with open('registers.json', 'r') as f:
         REGDICT = json.load(f)
-    with open('type.json', 'r') as f:
-        TYPE_DICT = json.load(f)
-    print(REGDICT)
-    print(INSTRUCTION_DICT)
-    print(TYPE_DICT)
     process(sys.argv[1])
--- Assgn_7_Grp_1/assembler/parse_test.s
+++ Assgn_7_Grp_1/assembler/parse_test.s
@@ -1,91 +1,115 @@
-###############################################################################
-    # Assignment Number: 2
-    # Problem Number: 2 
-    # Semester Number: 5
-    # Group Number: 1 
-    # Group Members: Animesh Jha(19CS10070), Nisarg Upadhyaya (19CS30031)
-###############################################################################
+# ###############################################################################
+#     # Assignment Number: 2
+#     # Problem Number: 2 
+#     # Semester Number: 5
+#     # Group Number: 1 
+#     # Group Members: Animesh Jha(19CS10070), Nisarg Upadhyaya (19CS30031)
+# ###############################################################################
 
-.globl  main
-.data
+# .globl  main
+# .data
 
-# program output text constants
-prompt:
-    .asciiz "Please enter a positive integer: "
-result:
-    .asciiz "The gcd of the two numbers is: "
-neg_int_err:
-    .asciiz "Error: Negative integer entered, terminating the program."
-newline:
-    .asciiz "\n"
+# # program output text constants
+# prompt:
+#     .asciiz "Please enter a positive integer: "
+# result:
+#     .asciiz "The gcd of the two numbers is: "
+# neg_int_err:
+#     .asciiz "Error: Negative integer entered, terminating the program."
+# newline:
+#     .asciiz "\n"
 
-.text
+# .text
 
-# main program
-#
-# program variables
-#   a:   $s0
-#   b:   $s1
-#   g:   $s2
+# # main program
+# #
+# # program variables
+# #   a:   $s0
+# #   b:   $s1
+# #   g:   $s2
 
-main:
-    li      $v0, 4          # issue input prompt
-    la      $a0, prompt
-    syscall
+# main:
+#     li      $v0, 4          # issue input prompt
+#     la      $a0, prompt
+#     syscall
 
-    li      $v0, 5          # get a from user
-    syscall
-    move    $s0, $v0
+#     li      $v0, 5          # get a from user
+#     syscall
+#     move    $s0, $v0
 
-    li      $v0, 4          # issue input prompt
-    la      $a0, prompt
-    syscall
+#     li      $v0, 4          # issue input prompt
+#     la      $a0, prompt
+#     syscall
 
-    li      $v0, 5          # get b from user
-    syscall
-    move    $s1, $v0
+#     li      $v0, 5          # get b from user
+#     syscall
+#     move    $s1, $v0
 
-    # check if a is positive
-    bltz    $s0, neg_inp
-    # check if b is positive
-    bltz    $s1, neg_inp
+#     # check if a is positive
+#     bltz    $s0, neg_inp
+#     # check if b is positive
+#     bltz    $s1, neg_inp
 
-    add $s2, $s1, $zero    # default value of gcd = b
-    beqz $s0, return_gcd    # if a=0 then return gcd as b
+#     add $s2, $s1, $zero    # default value of gcd = b
+#     beqz $s0, return_gcd    # if a=0 then return gcd as b
 
-gcd_loop:
+# gcd_loop:
     
-    move $s2, $s0   # set gcd = a 
+#     move $s2, $s0   # set gcd = a 
 
-    beqz $s1, return_gcd    # check if b is 0
+#     beqz $s1, return_gcd    # check if b is 0
 
-    bgt $s0, $s1, a_gt_b    # check if a>b  then a = a-b
-    sub $s1, $s1, $s0   # else b = b-a
-    j gcd_loop
+#     bgt $s0, $s1, a_gt_b    # check if a>b  then a = a-b
+#     sub $s1, $s1, $s0   # else b = b-a
+#     j gcd_loop
     
-a_gt_b:
-    sub $s0, $s0, $s1   # a = a-b
-    j gcd_loop
+# a_gt_b:
+#     sub $s0, $s0, $s1   # a = a-b
+#     j gcd_loop
 
-return_gcd:
-    li      $v0, 4          # print result string
-    la      $a0, result
-    syscall
+# return_gcd:
+#     li      $v0, 4          # print result string
+#     la      $a0, result
+#     syscall
     
-    li      $v0, 1           # print gcd
-    move      $a0, $s2
-    syscall
-    j end
+#     li      $v0, 1           # print gcd
+#     move      $a0, $s2
+#     syscall
+#     j end
 
-neg_inp:
-    li      $v0, 4          # issue error prompt
-    la      $a0, neg_int_err
-    syscall
-    # will now go to end_program
+# neg_inp:
+#     li      $v0, 4          # issue error prompt
+#     la      $a0, neg_int_err
+#     syscall
+#     # will now go to end_program
 
-end:
-    li      $v0, 4          # add newline
-    la      $a0, newline
-    syscall
-    li      $v0, 10         # terminate program
-    syscall
+# end:
+#     li      $v0, 4          # add newline
+#     la      $a0, newline
+#     syscall
+#     li      $v0, 10         # terminate program
+#     syscall
+# ###############################################################################
+
+add $5, $6
+comp $5, $6
+addi $5, -15
+compi $5, 15
+and $5, $6
+xor $5, $6
+shll $5,9
+shrl $5,9
+shllv $5, $6
+shrl $5, $6
+shra $5, 9
+shrav  $5, $6
+lw $5, -9
+sw $5, -9
+b 14
+br $5
+bltz $5, 14
+bz $5, 14
+bnz $5, 14
+bl 14
+bcy 14
+bncy 14
